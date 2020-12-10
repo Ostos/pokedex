67642c0af96b7efd4a5df5ceb70e2d09430d9ab4
Gerardo Ostos <gog.ostos@gmail.com>
2020-12-09 22:11:35 -0500

    store all pokemon list in localstorage

--- src/App.js
+++ src/App.js
@@ -3,36 +3,33 @@ import MainPage from './components/MainPage/MainPage';
 import DetailPage from './components/DetailPage/DetailPage';
 import './App.css';
 import { detailPage, mainPage } from './utils/routingPaths';
-// import pokedexService from './services/pokedexService';
+import pokedexService from './services/pokedexService';
 import React, { useEffect, useState } from 'react';
 
 function App() {
 
   const [allPokemon, setAllPokemon] = useState([]);
 
+  async function storeAllPokemon() {
+    const storedPokemon = localStorage.getItem('pokedex-all-pokemon');
+    if(!storedPokemon) {
+      const service = new pokedexService();
+      const pokemon = await service.getAllPokemon();
+      localStorage.setItem('pokedex-all-pokemon', JSON.stringify(pokemon.results));
+      setAllPokemon(pokemon);
+    } else {
+      setAllPokemon(JSON.parse(storedPokemon));
+    }
+  }
+
   useEffect(() => {
-    // const service = new pokedexService();
-    // const pokemon = service.getAllPokemon();
-    const pokemon = [
-      { name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
-      { name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/" },
-      { name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3/" },
-      { name: "charmander", url: "https://pokeapi.co/api/v2/pokemon/4/" },
-      { name: "charmeleon", url: "https://pokeapi.co/api/v2/pokemon/5/" },
-      { name: "charizard", url: "https://pokeapi.co/api/v2/pokemon/6/" }
-    ];
-    console.log(pokemon);
-    setAllPokemon(pokemon);
+    storeAllPokemon();
    }, []);
 
   return (
     <div className="Pokedex">
       <BrowserRouter>
         <Switch>
-          <Route
-            path={mainPage}
-            render={() => <MainPage allPokemon={allPokemon} />}
-          />
           <Route
             path={detailPage}
             render={() => <DetailPage />}
