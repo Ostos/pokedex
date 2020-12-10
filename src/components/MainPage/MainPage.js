91ee459431ed013616cbfab533c7597ab59b21a8
Gerardo Ostos <gog.ostos@gmail.com>
2020-12-10 00:58:36 -0500

    filter by toggle all and bag

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PokemonCard from "../PokemonCard/PokemonCard";
 import ToggleButton from "../ToggleButton/ToggleButton";
 import "./MainPage.scss";
@@ -5,23 +6,29 @@ import "./MainPage.scss";
 const MainPage = (props) => {
     const { allPokemon } = props;
 
+    const [pokemonToRender, setPokemonToRender] = useState(allPokemon);
+
+    function renderAllPokemon() {
+        setPokemonToRender(allPokemon);
+    }
+
+    function renderPokemonInBag() {
+        setPokemonToRender([allPokemon[0]]); // NEEDS WORK: Filter them
+    }
+
     return(
         <div className="MainPage">
             <div className="MainPage__filters">
                 <ToggleButton
                     name1="All"
                     name2="Bag"
-                    handler1={() => {
-                        console.log("Show all pokemon");
-                    }}
-                    handler2={() => {
-                        console.log("Show pokemon in the bag");
-                    }}
+                    handler1={renderAllPokemon}
+                    handler2={renderPokemonInBag}
                 />
                 <div><input type="text" placeholder="Search..." /></div>
             </div>
             <div className="MainPage__cards">
-                {allPokemon.map(pokemon => {
+                {pokemonToRender.map(pokemon => {
                     return <PokemonCard key={pokemon.name} pokemon={pokemon} />
                 })}
             </div>
