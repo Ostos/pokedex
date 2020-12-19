f185c6e77381f6d428fb0fdc766a566318befc5e
Gerardo Ostos <gog.ostos@gmail.com>
2020-12-19 02:30:27 -0600

    get pokemon data when accessing DetailPage when no data exists

--- src/components/DetailPage/DetailPage.js
+++ src/components/DetailPage/DetailPage.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
-import "./DetailPage.scss";
+import { Fragment, useEffect, useState } from "react";
 import GoogleMapReact from 'google-map-react';
 import PokedexService from "../../services/pokedexService";
-import { Redirect, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import localStore from '../../utils/store';
+import "./DetailPage.scss";
 
 const MapsMarker = (props) => {
     return(
@@ -19,7 +19,6 @@ const DetailPage = () => {
     const [isInBag, setIsInBag] = useState(false);
     const [locations, setLocations] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [redirect, setRedirect] = useState(false);
     const API_KEY = 'AIzaSyCcpUwGDDw_x4yR4Ng-fbS5CfcJGr5CG6A';
     const { name } = useParams();
 
@@ -43,75 +42,86 @@ const DetailPage = () => {
     }
 
     useEffect(() => {
-        const pokemon = localStore.get(`pokemon-${name}`);
-        if(!pokemon) {
-            setRedirect(true);
-        } else {
-            setPokemon(pokemon);
-            setIsInBag(pokemon.inBag);
-            loadPokemonLocations(pokemon.id);
+        async function getPokemon() {
+            const service = new PokedexService();
+            const pokemon = await service.getPokemon({
+                name: name, 
+                url: `https://pokeapi.co/api/v2/pokemon/${name}`
+            });
+            if(pokemon instanceof Error) {
+                setPokemon(null);
+            } else {
+                setPokemon(pokemon);
+                setIsInBag(pokemon.inBag);
+                loadPokemonLocations(pokemon.id);
+            }
         }
-    }, [name]);
 
-    if(redirect) {
-        return(
-            <Redirect to='/' />
-        );
-    }
+        getPokemon();
+    }, [name]);
 
     return(
         <div className="DetailPage">
-            <div className="DetailPage__info">
-                <div className="DetailPage__info__profile-picture">
-                    <img
-                        src={pokemon.imageUrl}
-                        alt={name}
-                    />
-                    <span>{pokemon.name}</span>
+            {pokemon === null && (
+                <div className="DetailPage__error-message">
+                    Oops! That pokemon doesn't exist!
                 </div>
-                <div className="DetailPage__info__in-bag">
-                    <b>In Bag</b> <input type="checkbox" onChange={setInBag} value={isInBag} checked={isInBag} />
-                </div>
-                <div className="DetailPage__info__description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida augue turpis, vel molestie magna lobortis ac. Aliquam erat volutpat. Phasellus turpis quam, fermentum nec lacus feugiat, accumsan fringilla odio. Pellentesque at laoreet lectus. Pellentesque molestie elit eget massa tempor, ac ornare felis posuere. Cras ac elementum lacus, at pharetra purus. Integer a maximus arcu, quis euismod massa. Aliquam in commodo ligula, eu sagittis lacus. Donec pretium magna at tortor luctus posuere. In hac habitasse platea dictumst. Sed nec dictum purus, vitae tristique orci. Nunc sed arcu non nisl ornare consectetur eget consequat enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin imperdiet imperdiet lacus, eu vestibulum elit accumsan eget.
-                </div>
-                <div className="DetailPage__info__height">
-                    <b>Height:</b> {pokemon.height}
-                </div>
-                <div className="DetailPage__info__weight">
-                    <b>Weight:</b> {pokemon.weight}
-                </div>
-                <div className="DetailPage__info__types">
-                    <b>Types:</b> {pokemon.types}
-                </div>
-                <div className="DetailPage__info__abilities">
-                    <b>Abilities:</b> {pokemon.abilities}
-                </div>
-            </div>
-            <div className="DetailPage__location">
-                <div className="DetailPage__location__map">
-                    {!isLoading && (
-                        <GoogleMapReact
-                            bootstrapURLKeys={{ key: API_KEY }}
-                            defaultCenter={locations[0] || { lat: 32.715736, lng: -117.161087 }}
-                            defaultZoom={7}
-                        >
-                            {locations.length > 0 && locations.map((location, index) => {
-                                return (
-                                    <MapsMarker
-                                        key={index}
-                                        image={pokemon.imageUrl}
-                                        {...location}
-                                    />
-                                )
-                            })}
-                        </GoogleMapReact>
-                    )}
-                    {isLoading && (
-                        <div className="DetailPage__location__map__loading">Loading...</div>
-                    )}
-                </div>
-            </div>
+            )}
+            {pokemon !== null && (
+                <Fragment>
+                    <div className="DetailPage__info">
+                        <div className="DetailPage__info__profile-picture">
+                            <img
+                                src={pokemon.imageUrl}
+                                alt={name}
+                            />
+                            <span>{pokemon.name}</span>
+                        </div>
+                        <div className="DetailPage__info__in-bag">
+                            <b>In Bag</b> <input type="checkbox" onChange={setInBag} value={isInBag} checked={isInBag} />
+                        </div>
+                        <div className="DetailPage__info__description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida augue turpis, vel molestie magna lobortis ac. Aliquam erat volutpat. Phasellus turpis quam, fermentum nec lacus feugiat, accumsan fringilla odio. Pellentesque at laoreet lectus. Pellentesque molestie elit eget massa tempor, ac ornare felis posuere. Cras ac elementum lacus, at pharetra purus. Integer a maximus arcu, quis euismod massa. Aliquam in commodo ligula, eu sagittis lacus. Donec pretium magna at tortor luctus posuere. In hac habitasse platea dictumst. Sed nec dictum purus, vitae tristique orci. Nunc sed arcu non nisl ornare consectetur eget consequat enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin imperdiet imperdiet lacus, eu vestibulum elit accumsan eget.
+                        </div>
+                        <div className="DetailPage__info__height">
+                            <b>Height:</b> {pokemon.height}
+                        </div>
+                        <div className="DetailPage__info__weight">
+                            <b>Weight:</b> {pokemon.weight}
+                        </div>
+                        <div className="DetailPage__info__types">
+                            <b>Types:</b> {pokemon.types}
+                        </div>
+                        <div className="DetailPage__info__abilities">
+                            <b>Abilities:</b> {pokemon.abilities}
+                        </div>
+                    </div>
+                    <div className="DetailPage__location">
+                        <div className="DetailPage__location__map">
+                            {!isLoading && (
+                                <GoogleMapReact
+                                    bootstrapURLKeys={{ key: API_KEY }}
+                                    defaultCenter={locations[0] || { lat: 32.715736, lng: -117.161087 }}
+                                    defaultZoom={7}
+                                >
+                                    {locations.length > 0 && locations.map((location, index) => {
+                                        return (
+                                            <MapsMarker
+                                                key={index}
+                                                image={pokemon.imageUrl}
+                                                {...location}
+                                            />
+                                        )
+                                    })}
+                                </GoogleMapReact>
+                            )}
+                            {isLoading && (
+                                <div className="DetailPage__location__map__loading">Loading...</div>
+                            )}
+                        </div>
+                    </div>
+                </Fragment>
+            )}
         </div>
     );
 };
